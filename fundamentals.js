c8fd1597abb61c9a4d5f1e61ceeb3a59ee6db32e
Karl Stolley <karl.stolley@gmail.com>
2021-09-01 14:14:16 -0500

    Add demos from Monday 8/29

---
+++ fundamentals.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var size = 'large';
+const person = {
+  age: ,
+  size: ,
+  favorites: []
+}
+var sayHello = function(name) {
+  console.log('Hello,', name);
+}
+var self = "I am me!";
+
+console.log('Hello world!');
+
+console.log('Hello again!');
+
+console.log('The size is', size);
+
+sayHello('Karl');
+
+console.log('The size is', size);
